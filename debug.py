06e6bcb5df70a4df7db13adbb3e6045b911f1f05
Dhaniar Dava Refaldo <aldodava55@gmail.com>
2025-09-14 14:38:21 +0700

    Debug token dari Railway

--- debug.py
+++ debug.py
@@ -1,3 +1,3 @@
 import os
-print("DISCORD_TOKEN:", repr(os.getenv("DISCORD_TOKEN")))
-print("CHANNEL_ID :", repr(os.getenv("CHANNEL_ID")))
+token = os.getenv("DISCORD_TOKEN")
+print("Token:", repr(token))
